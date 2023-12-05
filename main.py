9947f4031944ab3aaf2cb7b5cd54c659c9699655
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2023-12-04 23:33:41 -0700

    solve day 1 part 2

--- main.py
+++ main.py
@@ -1,39 +1,85 @@
+import collections
 import os
+import re
+
+NumberIndex = collections.namedtuple("NumberIndex", ["index", "number"])
 
 
 class Day01:
+    NUMBERS = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
     @classmethod
-    def solve(cls, input):
+    def part1(cls, input) -> int:
         input = input.split(os.linesep)
         input = [line.strip() for line in input]
         input = [line for line in input if line]
         return sum(cls._process_line(line) for line in input)
 
     @classmethod
-    def _find_first_digit(cls, string):
+    def part2(cls, input) -> int:
+        input = input.split(os.linesep)
+        input = [line.strip() for line in input]
+        input = [line for line in input if line]
+        return sum(cls._process_line_part2(line) for line in input)
+
+    @classmethod
+    def _process_line(cls, line) -> int:
+        first = cls._find_first_digit(line)
+        last = cls._find_last_digit(line)
+        return int(str(first) + str(last))
+
+    @classmethod
+    def _process_line_part2(cls, line) -> int:
+        numbers = cls._get_all_digits_on_line(line)
+        return int(str(numbers[0].number) + str(numbers[-1].number))
+
+    @classmethod
+    def _get_all_digits_on_line(cls, line: str) -> list[NumberIndex]:
+        result: list[NumberIndex] = []
+
+        for word, num in cls.NUMBERS.items():
+            matches = re.finditer(word, line)
+            result += [NumberIndex(match.start(), num) for match in matches]
+
+        digit_matches = re.finditer(r"\d", line)
+        result += [
+            NumberIndex(match.start(), int(match.group())) for match in digit_matches
+        ]
+
+        result.sort()
+        return result
+
+    @classmethod
+    def _find_first_digit(cls, string) -> int:
         for c in string:
             try:
                 return int(c)
-            except:
+            except ValueError:
                 continue
 
+        return 0
+
     @classmethod
-    def _find_last_digit(cls, string):
+    def _find_last_digit(cls, string) -> int:
         return cls._find_first_digit(reversed(string))
-
-    @classmethod
-    def _process_line(cls, line):
-        first = cls._find_first_digit(line)
-        last = cls._find_last_digit(line)
-
-        return int(str(first) + str(last))
 
 
 def main():
     with open("inputs/day01.txt") as file:
         input = file.read()
 
-    print(Day01.solve(input))
+    print(Day01.part1(input))
+    print(Day01.part2(input))
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from main import Day01
 
 
 class Day01Tests(unittest.TestCase):
-    def test_day01(self):
+    def test_day1(self):
         input = textwrap.dedent(
         """
             1abc2
@@ -15,14 +15,49 @@ class Day01Tests(unittest.TestCase):
         """
         )
 
-        self.assertEqual(Day01.solve(input), 142)
+        self.assertEqual(Day01.part1(input), 142)
 
-    def test_day01_find_digit(self):
+    def test_day2(self):
+        input = textwrap.dedent(
+        """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """
+        )
+
+        self.assertEqual(Day01.part2(input), 281)
+
+    def test_find_digit(self):
         input = "1abc2"
         self.assertEqual(Day01._find_first_digit(input), 1)
         self.assertEqual(Day01._find_last_digit(input), 2)
 
-    def test_day01_find_digit_notends(self):
+    def test_find_digit_notends(self):
         input = "treb7uchet"
         self.assertEqual(Day01._find_first_digit(input), 7)
         self.assertEqual(Day01._find_last_digit(input), 7)
+
+    def test_process_line_part2(self):
+        input = "two1nine"
+        self.assertEqual(Day01._process_line_part2(input), 29)
+
+    def test_process_line_part2_overlap(self):
+        input = "eightwothree"
+        self.assertEqual(Day01._process_line_part2(input), 83)
+
+    def test_get_all_digits_one_word(self):
+        input = "eight"
+        self.assertEqual(Day01._get_all_digits_on_line(input), [(0, 8)])
+
+    def test_get_all_digits_one_word_and_number(self):
+        input = "eight2"
+        self.assertEqual(Day01._get_all_digits_on_line(input), [(0, 8), (5, 2)])
+
+    def test_mean_case_that_started_this_whole_thing(self):
+        input = "eightwothree"
+        self.assertEqual(Day01._get_all_digits_on_line(input), [(0, 8), (4, 2), (7, 3)])
