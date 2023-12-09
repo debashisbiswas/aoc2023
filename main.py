1f076f69634e498ca6d98e6495a0e46652004251
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2023-12-09 13:10:46 -0700

    add timing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dataclasses import dataclass
+from time import time
 from types import ModuleType
 
 from advent import day01, day02, day03, day04
@@ -14,13 +15,17 @@ class Day:
 
 def create_header_string() -> str:
     parts = []
-    parts += [f"         | Part 1     | Part 2    "]
-    parts += [f"---------+------------+-----------"]
+    parts += [f"         | Part 1     | (time)     | Part 2     | (time)     "]
+    parts += [f"---------+------------+------------+------------+------------"]
     return os.linesep.join(parts)
 
 
-def create_solution_string(title, part_one, part_two) -> str:
-    return f"{title:8} | {str(part_one).ljust(10)} | {str(part_two).ljust(10)}"
+def create_solution_string(
+    title, part_one, part_two, part_one_time: float, part_two_time: float
+) -> str:
+    part_one_time_str = f"{round(part_one_time, 5)}s"
+    part_two_time_str = f"{round(part_two_time, 5)}s"
+    return f"{title:8} | {str(part_one).ljust(10)} | {str(part_one_time_str).ljust(10)} | {str(part_two).ljust(10)} | {str(part_two_time_str).ljust(10)}"
 
 
 def print_solutions(days: list[Day]):
@@ -30,9 +35,22 @@ def print_solutions(days: list[Day]):
         with open(day.input_filename) as file:
             input = file.read()
 
+        part_one_start = time()
         part_one_solution = day.module.part1(input)
+        part_one_end = time()
+
+        part_two_start = time()
         part_two_solution = day.module.part2(input)
-        print(create_solution_string(day.title, part_one_solution, part_two_solution))
+        part_two_end = time()
+        print(
+            create_solution_string(
+                day.title,
+                part_one_solution,
+                part_two_solution,
+                part_one_end - part_one_start,
+                part_two_end - part_two_start,
+            )
+        )
 
 
 def main():
